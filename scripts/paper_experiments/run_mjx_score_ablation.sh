cb1755c2afe9b7f4085e404d0c5f6b6535d0817c
Claas Voelcker <claas@voelcker.net>
2025-09-23 14:48:31 -0400

    Updated score experiments

--- scripts/paper_experiments/run_mjx_score_ablation.sh
+++ scripts/paper_experiments/run_mjx_score_ablation.sh
@@ -1,3 +1,6 @@
 # mjx_dmc
-sbatch scripts/paper_experiments/slurm_dmc_score_ablation.sh gae mjx_dmc_large_data large,score_gae,scaled
-sbatch scripts/paper_experiments/slurm_dmc_score_ablation.sh q mjx_dmc_large_data large,score_q,scaled
+# sbatch scripts/paper_experiments/slurm_dmc_score_ablation.sh mjx_dmc_large_data pathwise_q False  large,pathwise
+# sbatch scripts/paper_experiments/slurm_dmc_score_ablation.sh mjx_dmc_large_data score_based_q False  large,score3_q,not_scaled
+# sbatch scripts/paper_experiments/slurm_dmc_score_ablation.sh mjx_dmc_large_data score_based_q True  large,score3_q
+sbatch scripts/paper_experiments/slurm_dmc_score_ablation.sh mjx_dmc_large_data score_based_gae False large,score4_gae
+sbatch scripts/paper_experiments/slurm_dmc_score_ablation.sh mjx_dmc_large_data score_based_gae False large,score4_gae
--- scripts/paper_experiments/slurm_dmc_score_ablation.sh
+++ scripts/paper_experiments/slurm_dmc_score_ablation.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #SBATCH -N 1            # number of nodes on which to run
 #SBATCH --gres=gpu:1        # number of gpus
-#SBATCH --cpus-per-task=8     # number of cpus required per task
-#SBATCH --mem=32GB
+#SBATCH --cpus-per-task=16     # number of cpus required per task
+#SBATCH --mem=128GB
 #SBATCH --ntasks=1
 #SBATCH --tasks-per-node=1
-#SBATCH --time=1:00:00      # time limit
+#SBATCH --time=2:00:00      # time limit
 #SBATCH --account aip-gigor
 #SBATCH --job-name=mpsac_val
 #SBATCH --output=slurm_logs/slurm_mjx_op_%A_%a.out
 #SBATCH --error=slurm_logs/slurm_mjx_op_%A_%a.err
-#SBATCH --exclude=kn104,kn115,kn146,kn153
 #SBATCH --array=0-230%23
 
 env=(AcrobotSwingup AcrobotSwingupSparse BallInCup CartpoleBalance CartpoleBalanceSparse CartpoleSwingup CartpoleSwingupSparse CheetahRun FingerSpin FingerTurnEasy FingerTurnHard FishSwim HopperHop HopperStand PendulumSwingup ReacherEasy ReacherHard WalkerRun WalkerWalk WalkerStand HumanoidStand HumanoidWalk HumanoidRun)
+# env=(HumanoidStand HumanoidWalk HumanoidRun)
 hostname
 
 # cd /home/$USER/projects/aip-gigor/voelcker/reppo
@@ -25,5 +25,7 @@ uv run src/train.py --config-name=reppo_continuous \
     env.name=${env[$((SLURM_ARRAY_TASK_ID%23))]} \
 	+experiments=$1 \
 	seed=$RANDOM \
-	algorithm.gradient_estimator=score_based_$2 \
-	tags=[score_based,mjx,paper_adamw,$3]
+	algorithm.gradient_estimator=$2 \
+	algorithm.scale_samples_with_action_d=$3 \
+	algorithm.fixed_actor_std=True \
+	tags=[score_based,mjx,paper_adamw,$4]
